a2f8f7bbea4eeb3b1812ccbe18873773e5b0170e
brielmohoms <brielmohoms@yahoo.com>
2025-01-16 22:42:14 +0100

    Updated backend configuration

--- tagebuch-backend/db.js
+++ tagebuch-backend/db.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/journal');
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Database connection failed:', error);
