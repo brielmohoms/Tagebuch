1081bdc0d17ee71bca2f5d567951b6b8e9019bc1
brielmohoms <brielmohoms@yahoo.com>
2025-02-07 12:23:12 +0100

    404 not found error fix

--- tagebuch-frontend/src/App.js
+++ tagebuch-frontend/src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './Layout/layout';
